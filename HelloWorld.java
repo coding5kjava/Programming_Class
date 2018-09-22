bcf33914fc4bf10e53d3d89831de41071ce133d6
coding5kjava <43100373+coding5kjava@users.noreply.github.com>
2018-09-22 12:36:35 -0700

    First Commit
    First Ever Commit

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+  public static void main (String [] args ){
+    System.out.println("Hello World");
+    String s = "hello world";
+    System.out.println(s);
+    
+    int number = 1;
+    System.out.println (number);
+    System.out.println (1);
+    
+    int number2 = 6;
+    System.out.println (number+number2);
+    System.out.println ((double)number/number2);
+  }
+}
